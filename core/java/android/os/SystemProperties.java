da57e073d0534560e6ff7fa358c24348698ba48d
SunRain <5908183@163.com>
2012-08-02 04:33:42 +0800

    STS moto phone changes
    Change-Id: Iae8acab9ab812b3353ec9d09d8c074bb1c34fa12

--- core/java/android/os/SystemProperties.java
+++ core/java/android/os/SystemProperties.java
@@ -34,10 +34,6 @@ public class SystemProperties
     public static final int PROP_VALUE_MAX = 91;
     public static final boolean OMAP_ENHANCEMENT = native_get_boolean("com.ti.omap_enhancement", false);
 
-    public static final boolean QCOM_HARDWARE = native_get_boolean("com.qc.hardware", false);
-
-    public static final boolean NFCTI_ENHANCEMENT = native_get_boolean("com.ti.nfcti_enhancement", false);
-
     private static final ArrayList<Runnable> sChangeCallbacks = new ArrayList<Runnable>();
 
     private static native String native_get(String key);
