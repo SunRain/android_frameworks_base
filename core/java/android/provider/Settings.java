d50496778f086133b48b27265159ff4070818224
SunRain <41245110@qq.com>
2012-10-15 22:26:51 +0800

    Policy : remove the basic Nav controls in Power Dialog, really useless an ugly
    Change-Id: I43ddf06fdda8a8e57e89a34d831affd49c11ab31

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3184,12 +3184,6 @@ public final class Settings {
          */
         public static final String TABLET_UI = "tablet_ui";
 
-        /**
-         * Show the NavBar dialog in Power menu
-         * @hide
-         */
-        public static final String POWER_DIALOG_SHOW_NAVBAR_HIDE = "power_dialog_show_navbar_hide";
-
          /**
 		  * Used to determine if the NavBar should be enabled on devices that do not
           * otherwise have a NavBar
